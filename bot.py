16eade052ad5f29c6ea5af3e571571cf54070875
Sen <sen@onee.ch>
2021-07-08 20:36:38 -0400

    first commit

---
+++ bot.py
@@ -0,0 +1,16 @@
+from dgg_chat import DGGChat
+
+chat = DGGChat()
+
+prefix = "!"
+    
+@chat.on_chat_message
+def on_chat_message(message):
+    if message.content.startswith(prefix):
+        if message.content.split()[0] == prefix+"sr":#command
+            url = message.content.split()[1]
+            sr = open("sr.txt",'a', encoding="utf-8")
+            if url.startswith("https://youtu"):
+                sr.write(url)
+                sr.close()
+chat.run_forever()
